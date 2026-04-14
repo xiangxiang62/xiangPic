fcacf438882d06958be503753f8ef873eae8d3f5
张鑫 <1964362087@qq.com>
2026-04-14 17:22:10 +0800

    feat: 前端新增登录注册页面

--- xiang_pic_frontend/src/pages/user/UserRegisterPage.vue
+++ xiang_pic_frontend/src/pages/user/UserRegisterPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="userRegisterPage">
-    <h2 class="title">鱼皮云图库 - 用户注册</h2>
+    <h2 class="title">享 · 云图库 - 用户注册</h2>
     <div class="desc">企业级智能协同云图库</div>
     <a-form
       :model="formState"
