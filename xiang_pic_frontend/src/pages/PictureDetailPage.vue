35621039ce7329128e51c13b52048b18d6ce8e21
张鑫 <1964362087@qq.com>
2026-04-14 17:23:34 +0800

    feat: 前端完善主页以及新增图片详情页

--- xiang_pic_frontend/src/pages/PictureDetailPage.vue
+++ xiang_pic_frontend/src/pages/PictureDetailPage.vue
@@ -1,145 +1,156 @@
 <template>
-  <div id="addPicturePage">
-    <h2 style="margin-bottom: 16px">
-      {{ route.query?.id ? '修改图片' : '创建图片' }}
-    </h2>
-    <PictureUpload :picture="picture" :onSuccess="onSuccess" />
-    <a-form v-if="picture" layout="vertical" :model="pictureForm" @finish="handleSubmit">
-      <a-form-item label="名称" name="name">
-        <a-input v-model:value="pictureForm.name" placeholder="请输入名称" />
-      </a-form-item>
-      <a-form-item label="简介" name="introduction">
-        <a-textarea
-          v-model:value="pictureForm.introduction"
-          placeholder="请输入简介"
-          :rows="2"
-          autoSize
-          allowClear
-        />
-      </a-form-item>
-      <a-form-item label="分类" name="category">
-        <a-auto-complete
-          v-model:value="pictureForm.category"
-          :options="categoryOptions"
-          placeholder="请输入分类"
-          allowClear
-        />
-      </a-form-item>
-      <a-form-item label="标签" name="tags">
-        <a-select
-          v-model:value="pictureForm.tags"
-          :options="tagOptions"
-          mode="tags"
-          placeholder="请输入标签"
-          allowClear
-        />
-      </a-form-item>
-      <a-form-item>
-        <a-button type="primary" html-type="submit" style="width: 100%">创建</a-button>
-      </a-form-item>
-    </a-form>
+  <div id="pictureDetailPage">
+    <a-row :gutter="[16, 16]">
+      <!-- 图片展示区 -->
+      <a-col :sm="24" :md="16" :xl="18">
+        <a-card title="图片预览">
+          <a-image
+            style="max-height: 600px; object-fit: contain"
+            :src="picture.url"
+          />
+        </a-card>
+      </a-col>
+      <!-- 图片信息区 -->
+      <a-col :sm="24" :md="8" :xl="6">
+        <a-card title="图片信息">
+          <a-descriptions :column="1">
+            <a-descriptions-item label="作者">
+              <a-space>
+                <a-avatar :size="24" :src="picture.user?.userAvatar" />
+                <div>{{ picture.user?.userName }}</div>
+              </a-space>
+            </a-descriptions-item>
+            <a-descriptions-item label="名称">
+              {{ picture.name ?? '未命名' }}
+            </a-descriptions-item>
+            <a-descriptions-item label="简介">
+              {{ picture.introduction ?? '-' }}
+            </a-descriptions-item>
+            <a-descriptions-item label="分类">
+              {{ picture.category ?? '默认' }}
+            </a-descriptions-item>
+            <a-descriptions-item label="标签">
+              <a-tag v-for="tag in picture.tags" :key="tag">
+                {{ tag }}
+              </a-tag>
+            </a-descriptions-item>
+            <a-descriptions-item label="格式">
+              {{ picture.picFormat ?? '-' }}
+            </a-descriptions-item>
+            <a-descriptions-item label="宽度">
+              {{ picture.picWidth ?? '-' }}
+            </a-descriptions-item>
+            <a-descriptions-item label="高度">
+              {{ picture.picHeight ?? '-' }}
+            </a-descriptions-item>
+            <a-descriptions-item label="宽高比">
+              {{ picture.picScale ?? '-' }}
+            </a-descriptions-item>
+            <a-descriptions-item label="大小">
+              {{ formatSize(picture.picSize) }}
+            </a-descriptions-item>
+          </a-descriptions>
+        </a-card>
+        <a-space wrap>
+          <a-button v-if="canEdit" type="default" @click="doEdit">
+            编辑
+            <template #icon>
+              <EditOutlined />
+            </template>
+          </a-button>
+          <a-button v-if="canEdit" danger @click="doDelete">
+            删除
+            <template #icon>
+              <DeleteOutlined />
+            </template>
+          </a-button>
+          <a-button type="primary" @click="doDownload">
+            免费下载
+            <template #icon>
+              <DownloadOutlined />
+            </template>
+          </a-button>
+        </a-space>
+      </a-col>
+
+
+    </a-row>
   </div>
 
 </template>
 
 <script setup lang="ts">
-import PictureUpload from "@/components/PictureUpload.vue";
-import {onMounted, reactive, ref} from "vue";
-import {useRoute, useRouter} from "vue-router";
-import {
-  editPictureUsingPost,
-  getPictureVoByIdUsingGet,
-  listPictureTagCategoryUsingGet
-} from "@/api/pictureController";
+import {deletePictureUsingPost, getPictureVoByIdUsingGet} from "@/api/pictureController";
+import {computed, onMounted, ref} from "vue";
 import {message} from "ant-design-vue";
-const picture = ref<API.PictureVO>()
-const pictureForm = reactive<API.PictureEditRequest>({})
+import {downloadImage, formatSize} from "@/utils";
+import {useLoginUserStore} from "@/stores/useLoginUserStore";
+import router from "@/router";
+import { saveAs } from "file-saver";
 
-/**
- * 上传成功
- * @param newPicture
- */
-const onSuccess = (newPicture: API.PictureVO) => {
-  picture.value = newPicture
-  pictureForm.name = newPicture.name
-}
-const router = useRouter()
-const categoryOptions = ref<string[]>([])
-const tagOptions = ref<string[]>([])
+const props = defineProps<{
+  id: string | number
+}>()
 
-/**
- * 提交表单
- * @param values
- */
-const handleSubmit = async (values: any) => {
-  const pictureId = picture.value?.id
-  if (!pictureId) {
-    return
-  }
-  const res = await editPictureUsingPost({
-    id: pictureId,
-    ...values,
-  })
-  if (res.data.code === 0 && res.data.data) {
-    message.success('创建成功')
-    // 跳转到图片详情页
-    router.push({
-      path: `/picture/${pictureId}`,
-    })
-  } else {
-    message.error('创建失败，' + res.data.message)
-  }
-}
-// 获取标签和分类选项
-const getTagCategoryOptions = async () => {
-  const res = await listPictureTagCategoryUsingGet()
-  if (res.data.code === 0 && res.data.data) {
-    // 转换成下拉选项组件接受的格式
-    tagOptions.value = (res.data.data.tagList ?? []).map((data: string) => {
-      return {
-        value: data,
-        label: data,
-      }
-    })
-    categoryOptions.value = (res.data.data.categoryList ?? []).map((data: string) => {
-      return {
-        value: data,
-        label: data,
-      }
-    })
-  } else {
-    message.error('加载选项失败，' + res.data.message)
-  }
-}
+const picture = ref<API.PictureVO>({})
 
-onMounted(() => {
-  getTagCategoryOptions()
-})
-
-const route = useRoute()
-
-// 获取老数据
-const getOldPicture = async () => {
-  // 获取数据
-  const id = route.query?.id
-  if (id) {
+// 获取图片详情
+const fetchPictureDetail = async () => {
+  try {
     const res = await getPictureVoByIdUsingGet({
-      id: id,
+      id: props.id,
     })
     if (res.data.code === 0 && res.data.data) {
-      const data = res.data.data
-      picture.value = data
-      pictureForm.name = data.name
-      pictureForm.introduction = data.introduction
-      pictureForm.category = data.category
-      pictureForm.tags = data.tags
+      picture.value = res.data.data
+    } else {
+      message.error('获取图片详情失败，' + res.data.message)
     }
+  } catch (e: any) {
+    message.error('获取图片详情失败：' + e.message)
   }
 }
 
 onMounted(() => {
-  getOldPicture()
+  fetchPictureDetail()
 })
+
+const loginUserStore = useLoginUserStore()
+// 是否具有编辑权限
+const canEdit = computed(() => {
+  const loginUser = loginUserStore.loginUser;
+  // 未登录不可编辑
+  if (!loginUser.id) {
+    return false
+  }
+  // 仅本人或管理员可编辑
+  const user = picture.value.user || {}
+  return loginUser.id === user.id || loginUser.userRole === 'admin'
+})
+
+
+// 编辑
+const doEdit = () => {
+  router.push('/add_picture?id=' + picture.value.id)
+}
+// 删除
+const doDelete = async () => {
+  const id = picture.value.id
+  if (!id) {
+    return
+  }
+  const res = await deletePictureUsingPost({ id })
+  if (res.data.code === 0) {
+    message.success('删除成功')
+  } else {
+    message.error('删除失败')
+  }
+}
+
+// 处理下载
+const doDownload = () => {
+  downloadImage(picture.value.url)
+}
+
 
 
 </script>
