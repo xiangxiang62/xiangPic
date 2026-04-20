e4cc7f9d783b9fe05d0d10643a22a54f600af271
张鑫 <1964362087@qq.com>
2026-04-20 16:12:30 +0800

    feat: 空间管理添加类型检索

--- xiang_pic_frontend/src/pages/admin/SpaceManagePage.vue
+++ xiang_pic_frontend/src/pages/admin/SpaceManagePage.vue
@@ -26,6 +26,15 @@
           allow-clear
         />
       </a-form-item>
+      <a-form-item label="空间类别" name="spaceType">
+        <a-select
+          v-model:value="searchParams.spaceType"
+          :options="SPACE_TYPE_OPTIONS"
+          placeholder="请输入空间类别"
+          style="min-width: 180px"
+          allow-clear
+        />
+      </a-form-item>
       <a-form-item label="用户 id" name="userId">
         <a-input v-model:value="searchParams.userId" placeholder="请输入用户 id" allow-clear />
       </a-form-item>
@@ -45,6 +54,10 @@
         <!-- 空间级别 -->
         <template v-if="column.dataIndex === 'spaceLevel'">
           <a-tag>{{ SPACE_LEVEL_MAP[record.spaceLevel] }}</a-tag>
+        </template>
+        <!-- 空间类别 -->
+        <template v-if="column.dataIndex === 'spaceType'">
+          <a-tag>{{ SPACE_TYPE_MAP[record.spaceType] }}</a-tag>
         </template>
         <!-- 使用情况 -->
         <template v-if="column.dataIndex === 'spaceUseInfo'">
@@ -85,6 +98,7 @@ import {
   listSpaceByPageUsingPost
 } from "@/api/spaceController";
 import {SPACE_LEVEL_ENUM, SPACE_LEVEL_MAP, SPACE_LEVEL_OPTIONS} from "@/constants/space";
+import {SPACE_TYPE_MAP, SPACE_TYPE_OPTIONS} from "@/constants/spaceUser";
 
 const columns = [
   {
@@ -99,6 +113,10 @@ const columns = [
   {
     title: '空间级别',
     dataIndex: 'spaceLevel',
+  },
+  {
+    title: '空间类别',
+    dataIndex: 'spaceType',
   },
   {
     title: '使用情况',
