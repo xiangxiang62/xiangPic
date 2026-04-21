73a5c92d3b2241ddba6a3c0513708c1d26b60d40
张鑫 <1964362087@qq.com>
2026-04-21 15:24:53 +0800

    feat: 团队空间编辑图片新增 WebSocket 推送实时交互信息

---
+++ xiang_pic_backend/src/main/java/com/xiang/pic/xiangPicBackend/manager/websocket/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.xiang.pic.xiangPicBackend.manager.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import javax.annotation.Resource;
+
+/**
+ * Ws 处理请求配置
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Resource
+    private PictureEditHandler pictureEditHandler;
+
+    @Resource
+    private WsHandshakeInterceptor wsHandshakeInterceptor;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // websocket
+        registry.addHandler(pictureEditHandler, "/ws/picture/edit")
+                .addInterceptors(wsHandshakeInterceptor)
+                .setAllowedOrigins("*");
+    }
+}
